53bf215bc7ad6c8dbd6dbbb77072048ef82397da
James Pederson <james@jpederson.com>
2015-03-22 20:40:47 -0500

    Adjust homepage one-third sized column styles, colors, and order.

--- page-front.php
+++ page-front.php
@@ -29,49 +29,55 @@ get_header();
 
 		<div class="home-thirds">
 
+			<div class="third events">
+				<h2><span>Events</span></h2>
+				<div class="third-content">
+					<article class="first">
+						<h4><a href="#">An Event Title</a></h4>
+						<p>January 3, 2015  3:00P - 4:00PM</p>
+					</article>
+					<article>
+						<h4><a href="#">An Event Title</a></h4>
+						<p>January 3, 2015  3:00P - 4:00PM</p>
+					</article>
+				</div>
+				<button class="home-third-button link-events"><span>All Link Events</span></button>
+				<div class="clearfix"></div>
+			</div>
+
 			<div class="third news">
-				<h2><span>The Link <span>News</span></span></h2>
-				<?php
-				query_posts( 'posts_per_page=3' );
-				if ( have_posts() ) {
-					$num = 1;
-					while ( have_posts() ) {
-						the_post();
-						?>
-				<article<?php print ( $num == 1 ? ' class="first"' : '' ); ?>>
-					<h4><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h4>
-					<?php the_excerpt() ?>
-				</article>
-						<?php
-						$num++;
-					} // end while
-				} // end if
-				?>
+				<h2><span>News</span></h2>
+				<div class="third-content">
+					<?php
+					query_posts( 'posts_per_page=3' );
+					if ( have_posts() ) {
+						$num = 1;
+						while ( have_posts() ) {
+							the_post();
+							?>
+					<article<?php print ( $num == 1 ? ' class="first"' : '' ); ?>>
+						<h4><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h4>
+						<?php the_excerpt() ?>
+					</article>
+							<?php
+							$num++;
+						} // end while
+					} // end if
+					?>
+				</div>
 				<button class="home-third-button link-news"><span>All Link News</span></button>
 				<div class="clearfix"></div>
 			</div>
 
-			<div class="third match">
-				<h2><span>CU <span>Matchup</span></span></h2>
-				<article>
-					<h4><a href="#">Looking for new ideas? Need a new income source?</a></h4>
-					<p>Use our CU Match Up Tool to find the best partners to help you increase non-interest income, help with collections, save on operating expenses and more!</p>
-				</article>
+			<div class="third tweets">
+				<h2><span>Tweets</span></h2>
+				<div class="third-content">
+					<article>
+						<h4><a href="#">Looking for new ideas? Need a new income source?</a></h4>
+						<p>Use our CU Match Up Tool to find the best partners to help you increase non-interest income, help with collections, save on operating expenses and more!</p>
+					</article>
+				</div>
 				<button class="home-third-button cu-match"><span>Find Your Match</span></button>
-				<div class="clearfix"></div>
-			</div>
-
-			<div class="third events">
-				<h2><span>Upcoming Link <span>Events</span></span></h2>
-				<article class="first">
-					<h4><a href="#">An Event Title</a></h4>
-					<p>January 3, 2015  3:00P - 4:00PM</p>
-				</article>
-				<article>
-					<h4><a href="#">An Event Title</a></h4>
-					<p>January 3, 2015  3:00P - 4:00PM</p>
-				</article>
-				<button class="home-third-button link-events"><span>All Link Events</span></button>
 				<div class="clearfix"></div>
 			</div>
 
