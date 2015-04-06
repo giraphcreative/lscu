35600819148df930ee4ee7c67c46f16f72665d89
James Pederson <james@jpederson.com>
2015-04-06 14:44:01 -0500

    Adjust footer styles - removing wrapper, setting up two columns for addresses when larger and 1220px wide.

--- footer.php
+++ footer.php
@@ -15,35 +15,33 @@
 	</section>
 	
 	<footer class="footer">
-		<div class="wrap">
-			<div class="column first">
-				<h3>Connect With Us</h3>
-				<p>22 Inverness Cntr Pky, #200<br>
-					Birmingham, Alabama</p>
-				<p>3692 Coolidge Court<br>
-					Tallahassee, Florida 32311</p>
-				<p><a href="tel:8662310545">866.231.0545</a></p>
-				<div class="social">
-					<a href="#"><img src="<?php bloginfo( 'template_url' ); ?>/img/social-twitter.png"></a><a href="#"><img src="<?php bloginfo( 'template_url' ); ?>/img/social-facebook.png"></a><a href="#"><img src="<?php bloginfo( 'template_url' ); ?>/img/social-google.png"></a><a href="#"><img src="<?php bloginfo( 'template_url' ); ?>/img/social-linkedin.png"></a><a href="#"><img src="<?php bloginfo( 'template_url' ); ?>/img/social-youtube.png"></a><a href="#"><img src="<?php bloginfo( 'template_url' ); ?>/img/social-pinterest.png"></a>
-				</div>
+		<div class="column first">
+			<h3>Connect With Us</h3>
+			<p class="address">22 Inverness Cntr Pky, #200<br>
+				Birmingham, Alabama</p>
+			<p class="address">3692 Coolidge Court<br>
+				Tallahassee, Florida 32311</p>
+			<p><a href="tel:8662310545">866.231.0545</a></p>
+			<div class="social">
+				<a href="#"><img src="<?php bloginfo( 'template_url' ); ?>/img/social-twitter.png"></a><a href="#"><img src="<?php bloginfo( 'template_url' ); ?>/img/social-facebook.png"></a><a href="#"><img src="<?php bloginfo( 'template_url' ); ?>/img/social-google.png"></a><a href="#"><img src="<?php bloginfo( 'template_url' ); ?>/img/social-linkedin.png"></a><a href="#"><img src="<?php bloginfo( 'template_url' ); ?>/img/social-youtube.png"></a><a href="#"><img src="<?php bloginfo( 'template_url' ); ?>/img/social-pinterest.png"></a>
 			</div>
-			<div class="column">
-				<h3>Links</h3>
-				<nav role="navigation">
-					<?php wp_nav_menu( array( 
-						'theme_location' => 'footer', 
-						'menu_class' => 'nav-menu' ) 
-					); ?>
-				</nav>
-			</div>
-			<div class="column">
-				<h3>Sign Up For News</h3>
-				<div class="subscribe">
-					<form action="#" method="post">
-						<input type="text" name="email" value="" placeholder="john@smith.com">
-						<input type="submit" name="submit" value="Subscribe">
-					</form>
-				</div>
+		</div>
+		<div class="column">
+			<h3>Links</h3>
+			<nav role="navigation">
+				<?php wp_nav_menu( array( 
+					'theme_location' => 'footer', 
+					'menu_class' => 'nav-menu' ) 
+				); ?>
+			</nav>
+		</div>
+		<div class="column last">
+			<h3>Sign Up For News</h3>
+			<div class="subscribe">
+				<form action="#" method="post">
+					<input type="text" name="email" value="" placeholder="john@smith.com">
+					<input type="submit" name="submit" value="Subscribe">
+				</form>
 			</div>
 		</div>
 	</footer><!-- #colophon -->
