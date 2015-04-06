d74470963b76f0d2e20e7c53676ee65c18649e73
James Pederson <james@jpederson.com>
2015-04-06 11:04:22 -0500

    Add widget class specific to widget type, set styles for sidebar menus, add sidebar class to quarter column on generic page template.

--- library/menus.php
+++ library/menus.php
@@ -12,10 +12,10 @@ if ( function_exists('register_sidebar') ) {
  	register_sidebar(array(
 		'name'=> 'General Sidebar',
 		'id' => 'sidebar-generic',
-        'before_widget' => '<div class="widget">',
+        'before_widget' => '<div class="widget %2$s">',
         'after_widget' => '</div>',
-        'before_title' => '<h4>',
-        'after_title' => '</h4>',
+        'before_title' => '<div class="widget-title"><h4>',
+        'after_title' => '</h4></div>',
     ));
 }
 
