142a0f8955725d684b3c0700bee2dd5a8ec27a7e
James Pederson <james@jpederson.com>
2015-05-20 15:47:05 -0500

    Login debugging.

--- library/login.php
+++ library/login.php
@@ -67,7 +67,7 @@ function login_form_shortcode( $atts, $content = null ) {
 	} 
 
 	$form .= get_bloginfo('home') . '/log-in/?redirect_to=';
-	$form = str_replace( get_bloginfo('home') . '/log-in/?redirect_to=', '', $form );
+	// $form = str_replace( get_bloginfo('home') . '/log-in/?redirect_to=', '', $form );
 
 	print $form; die;
 
