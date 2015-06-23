2020b3cf6def4878382a0d7692e32f4fcfe0ea08
James Pederson <james@jpederson.com>
2015-06-23 11:48:48 -0500

    Add single event template.

---
+++ single-event.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The Template for displaying all single posts
+ */
+
+get_header();
+
+?>
+	<div id="primary" class="site-content">
+
+		<div id="content" class="site-content content-narrow" role="main">
+			<div class="breadcrumbs" xmlns:v="http://rdf.data-vocabulary.org/#">
+			    <?php 
+			    if ( function_exists( 'bcn_display' ) ) {
+			        bcn_display();
+			    }
+			    ?>
+			</div>
+		<?php 
+		if ( have_posts() ) :
+			while ( have_posts() ) : the_post(); 
+				?>
+				<h1><?php the_title(); ?></h1>
+				<?php
+				the_content();
+			endwhile;
+		endif;
+		 ?>
+		</div><!-- #content -->
+
+	</div><!-- #primary -->
+<?php
+
+get_footer();
+
+?>
