28212eeb52ea0ff6f0c3a0704632bd4867257f54
James Pederson <james@jpederson.com>
2015-06-23 09:42:05 -0500

    Add post meta to category list template.

--- category.php
+++ category.php
@@ -44,6 +44,7 @@ $category_info = Taxonomy_MetaData::get( $taxonomy, $term_id );
 					?>
 					<article>
 					<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+					<p class="post-meta">Posted <?php the_date(); ?> in <?php the_category(', '); ?></p>
 					<?php the_excerpt(); ?>
 					</article>
 					<?php
