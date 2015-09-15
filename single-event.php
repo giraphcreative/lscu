d3454bc8c71a91c3a46be66382345adb46f3729d
James Pederson <james@jpederson.com>
2015-09-15 12:54:04 -0500

    Adjust time output to check for daylight savings time.

--- single-event.php
+++ single-event.php
@@ -36,12 +36,12 @@ get_header();
 					if ( has_cmb_value( 'event_end' ) ) {
 						print " - " . date( "g:i a", get_cmb_value( 'event_end' ) );
 					}
-					print " EST<br>";
+					print " E" . ( date('I') == 1 ? "S" : "D" ) . "T<br>";
 					print date( "g:i a", get_cmb_value( 'event_start' )-3600 );
 					if ( has_cmb_value( 'event_end' ) ) {
 						print " - " . date( "g:i a", get_cmb_value( 'event_end' )-3600 );
 					}
-					print " CST</p>";
+					print " C" . ( date('I') == 1 ? "S" : "D" ) . "T</p>";
 				}
 
 				// display the event duration.
