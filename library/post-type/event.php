0898412736ea85ce376d884dba133c74f7cd7253
James Pederson <james@jpederson.com>
2015-11-09 11:49:21 -0600

    Remove paragraph around the date in the homepage event list.

--- library/post-type/event.php
+++ library/post-type/event.php
@@ -636,7 +636,7 @@ class event_widget extends WP_Widget {
 		if ( !empty( $events ) ) {
 			foreach ( $events as $event ) {
 				print '<h4><a href="' . get_permalink( $event->ID ) . '">' . $event->post_title . '</a></h4>';
-				print '<p>' . date( 'n/j/Y g:ia', $event->_p_event_start ) . '</p>';
+				print date( 'n/j/Y g:ia', $event->_p_event_start );
 			}
 		}
 
