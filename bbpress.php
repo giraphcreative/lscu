195cc14d4f664686027ea1cffd4dbb73bc351891
James Pederson <james@jpederson.com>
2018-03-07 13:21:46 -0600

    Add custom forum template that has a sidebar widget area for the forums.

---
+++ bbpress.php
@@ -0,0 +1,44 @@
+<?php
+
+get_header();
+
+$color = get_cmb_value( 'large-title-color' );
+if ( empty( $color ) ) $color = 'navy';
+
+?>
+
+	<?php the_large_title(); ?>
+
+	<?php the_showcase(); ?>
+
+	<?php the_thumb_showcase(); ?>
+
+	<div id="content" class="wrap group content-two-column <?php print $color; ?>" role="main">
+		<div class="quarter sidebar">
+			<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar-forum') ) : ?><!-- no sidebar --><?php endif; ?>
+		</div>
+		<div class="three-quarter">
+			<div class="breadcrumbs" xmlns:v="http://rdf.data-vocabulary.org/#">
+			    <?php 
+			    if ( function_exists( 'bcn_display' ) ) {
+			        bcn_display();
+			    }
+			    ?>
+			</div>
+			<?php 
+			if ( have_posts() ) :
+				while ( have_posts() ) : the_post(); 
+					the_content();
+				endwhile;
+			endif;
+
+			the_accordion();
+			?>
+		</div>
+	</div><!-- #content -->
+
+<?php
+
+get_footer();
+
+?>
