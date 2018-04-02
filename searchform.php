f12e67499ed69d7177e17a788ebb3d6d99894043
James Pederson <james@jpederson.com>
2018-04-02 07:29:55 -0500

    Adjust styles on main logo and search form to accommodate ADA requirements.

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
 <form role="search" method="get" id="searchform" class="searchform" action="/" _lpchecked="1">
-	<input type="text" value="" name="s" id="s" placeholder="Search" title="Search this site.">
+	<label for="s">Search</label><input type="text" value="" name="s" id="s" placeholder="Search" title="Search this site.">
 	<input type="submit" id="searchsubmit" value="Search" title="Submit Search">
 </form>
