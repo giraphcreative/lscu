07c0694de815278eb7d3715f39d9b61fe7240d27
James Pederson <james@jpederson.com>
2019-04-01 14:38:41 -0500

    Don't output user data in the jwt-auth response.

--- library/api.php
+++ library/api.php
@@ -5,7 +5,6 @@ function jwt_auth_function( $data, $user ) {
 	$user_data = get_userdata( $user->data->ID );
 	$data['user_id'] = $user->data->ID;
 	$data['user_is_member'] = ( in_array( 'member', $user_data->roles ) ? true : false );
-	$data['user_data'] = $user_data;
 	return $data;
 }
 add_filter( 'jwt_auth_token_before_dispatch', 'jwt_auth_function', 10, 2 );
