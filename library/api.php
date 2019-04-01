317f4170fbe577425c554a5b6d40f2649af6d182
James Pederson <james@jpederson.com>
2019-04-01 15:07:07 -0500

    Add user meta data to the response array for the token auth.

--- library/api.php
+++ library/api.php
@@ -6,6 +6,7 @@ function jwt_auth_function( $data, $user ) {
 	$data['user_id'] = $user->data->ID;
 	$data['user_is_member'] = ( in_array( 'member', $user_data->roles ) ? true : false );
 	$data['user_data'] = $user_data;
+	$data['user_meta'] = get_user_meta( $user->data->ID );
 	return $data;
 }
 add_filter( 'jwt_auth_token_before_dispatch', 'jwt_auth_function', 10, 2 );
