3b8023c4533c4ba0747d59bc116addd7c3d24600
James Pederson <james@jpederson.com>
2019-04-10 17:20:41 -0500

    Add fields to the user profile page to manage the CU name and charter.

--- library/metabox.php
+++ library/metabox.php
@@ -9,6 +9,33 @@ require_once( 'cmb2/init.php' );
 // add metabox(es)
 function page_metaboxes( $meta_boxes ) {
 
+
+    $cmb_user = new_cmb2_box( array(
+        'id'               => CMB_PREFIX . 'edit',
+        'title'            => __( 'CU Information', 'cmb2' ), // Doesn't output for user boxes
+        'object_types'     => array( 'user' ), // Tells CMB2 to use user_meta vs post_meta
+        'show_names'       => true,
+        'new_user_section' => 'add-new-user', // where form will show on new user page. 'add-existing-user' is only other valid option.
+    ) );
+    $cmb_user->add_field( array(
+        'name'     => __( 'Credit Union Info', 'cmb2' ),
+        'desc'     => __( 'Manage the Credit Union Name and Charter ID for this user.', 'cmb2' ),
+        'id'       => 'extra_info',
+        'type'     => 'title',
+        'on_front' => false,
+    ) );
+    $cmb_user->add_field( array(
+        'name' => __( 'Credit Union Name', 'cmb2' ),
+        'id'   => 'cu_name',
+        'type' => 'text',
+    ) );
+    $cmb_user->add_field( array(
+        'name' => __( 'Credit Union Charter ID', 'cmb2' ),
+        'id'   => 'cu_charter_id',
+        'type' => 'text',
+    ) );
+
+
     global $colors, $event_types;
 
     // title metabox
