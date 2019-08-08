310c9650d2691fe99d1f4efbb38b619748a188f4
James Pederson <james@jpederson.com>
2019-08-08 17:26:55 -0500

    Update the API endpoint to give appropriate response for administrators as well as members.

--- library/api.php
+++ library/api.php
@@ -9,9 +9,13 @@ function jwt_auth_function( $data, $user ) {
 	// $data['user_data'] = $user_data;
 	// $data['user_meta'] = $user_meta;
 
+	$is_member = false;
+	if ( in_array( 'member', $user_data->roles ) ) $is_member = true;
+	if ( in_array( 'administrator', $user_data->roles ) ) $is_member = true;
+
 	// add our actual fields.
 	$data['user_id'] = $user->data->ID;
-	$data['user_is_member'] = ( in_array( 'member', $user_data->roles ) || in_array( 'administrator', $user_data->roles ) ? true : false );
+	$data['user_is_member'] = $is_member;
 	$data['user_fname'] = $user_meta['first_name'][0];
 	$data['user_lname'] = $user_meta['last_name'][0];
 	$data['cu_name'] = ( isset( $user_meta['cu_name'] ) ? $user_meta['cu_name'][0] : '' );
