592c2061f9d7ffb3292165966257656a7363f581
James Pederson <james@jpederson.com>
2019-08-08 17:14:24 -0500

    All check to see if the user is an admin - either way, we want it to allow them to log into InfoSight.

--- library/api.php
+++ library/api.php
@@ -11,7 +11,7 @@ function jwt_auth_function( $data, $user ) {
 
 	// add our actual fields.
 	$data['user_id'] = $user->data->ID;
-	$data['user_is_member'] = ( in_array( 'member', $user_data->roles ) ? true : false );
+	$data['user_is_member'] = ( in_array( 'member', $user_data->roles ) || in_array( 'administrator', $user_data->roles ) ? true : false );
 	$data['user_fname'] = $user_meta['first_name'][0];
 	$data['user_lname'] = $user_meta['last_name'][0];
 	$data['cu_name'] = ( isset( $user_meta['cu_name'] ) ? $user_meta['cu_name'][0] : '' );
